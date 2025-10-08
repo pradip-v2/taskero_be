e3f0f14ab91a395b839fd509884eadce75135edd
pradipthinkitive <pradip.bankar@thinkitive.com>
2025-10-08 17:47:45 +0530

    update parent and children tasks on updating the task is_done

--- taskero_be/tasks/models.py
+++ taskero_be/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db import transaction
 
 from taskero_be.core.models import BaseModel
 from taskero_be.projects.models import Project
@@ -20,5 +21,48 @@ class Task(BaseModel):
         blank=True,
         default=None,
         db_index=True,
+        related_name="subtasks",
     )
     level = models.PositiveIntegerField(default=1, db_index=True)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.pk}-{self.title}"
+
+    def save(self, *args, **kwargs):
+        # detect if task was marked as done
+        was_done_before = False
+        if self.pk:
+            old = (
+                Task.objects.filter(pk=self.pk)
+                .values_list("is_done", flat=True)
+                .first()
+            )
+            was_done_before = old
+
+        super().save(*args, **kwargs)
+
+        # if just changed to done, update all children recursively
+        if not was_done_before and self.is_done:
+            self.mark_children_done()
+
+        if not self.is_done:
+            self.mark_parent_not_done()
+
+    def mark_children_done(self):
+        """Recursively mark all nested children as done."""
+        subtasks = self.subtasks.all()
+        if not subtasks.exists():
+            return
+
+        with transaction.atomic():
+            for subtask in subtasks:
+                if not subtask.is_done:
+                    subtask.is_done = True
+                    subtask.save()
+
+    def mark_parent_not_done(self):
+        parent: Task = self.parent_task
+        if parent and parent.is_done:
+            parent.is_done = False
+            parent.save()
