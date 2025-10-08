fa87490061146b5440a873286e978571d28b8a56
pradipthinkitive <pradip.bankar@thinkitive.com>
2025-10-09 00:30:16 +0530

    add rest_framework_extensions

--- config/api_router.py
+++ config/api_router.py
@@ -1,15 +1,24 @@
-from django.conf import settings
-from rest_framework.routers import DefaultRouter
-from rest_framework.routers import SimpleRouter
+from rest_framework_extensions.routers import ExtendedSimpleRouter
 
 from taskero_be.projects.views import ProjectViewSet
+from taskero_be.tasks.views import ProjectTasksViewSet
 from taskero_be.tasks.views import TaskViewSet
 from taskero_be.users.api.views import UserViewSet
 
-router = DefaultRouter() if settings.DEBUG else SimpleRouter()
+router = ExtendedSimpleRouter()
+
 
 router.register("users", UserViewSet)
-router.register("projects", ProjectViewSet, basename="projects")
+router.register(
+    "projects",
+    ProjectViewSet,
+    basename="projects",
+).register(
+    "tasks",
+    ProjectTasksViewSet,
+    basename="project_tasks",
+    parents_query_lookups=["project"],
+)
 router.register("tasks", TaskViewSet, basename="tasks")
 
 
--- taskero_be/tasks/views.py
+++ taskero_be/tasks/views.py
@@ -1,4 +1,7 @@
+from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework.mixins import ListModelMixin
+from rest_framework_extensions.mixins import NestedViewSetMixin
 
 from taskero_be.core.decorators import add_created_by
 from taskero_be.core.decorators import add_updated_by
@@ -6,7 +9,13 @@ from taskero_be.tasks.models import Task
 from taskero_be.tasks.serializers import TaskSerializer
 
 
-class TaskViewSet(viewsets.ModelViewSet[Task]):
+class TaskViewSet(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet[Task],
+):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
@@ -17,3 +26,13 @@ class TaskViewSet(viewsets.ModelViewSet[Task]):
     @add_updated_by
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
+
+
+class ProjectTasksViewSet(
+    NestedViewSetMixin,
+    ListModelMixin,
+    viewsets.GenericViewSet[Task],
+):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    model = Task
